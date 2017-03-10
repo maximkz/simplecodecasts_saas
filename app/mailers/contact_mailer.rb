0c0e5c203ebbc28159507996c58fc72907051148
Maxim Prikhodko <max01kz@gmail.com>
2017-03-10 07:57:32 +0000

    Make changes to contactmailer and environment

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,10 +2,10 @@ class ContactMailer < ActionMailer::Base
     default to: 'max01kz@gmail.com'
     
    def contact_email(name, email, body)
-       @name = name
-       @email = email
-       @body = body
+        @name = name
+        @email = email
+        @body = body
        
-       mail(from: name, subject: 'Contact Form Message')
+        mail(from: name, subject: 'Contact Form Message')
     end
 end
