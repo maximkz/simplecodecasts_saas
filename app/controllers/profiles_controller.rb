51192f6a2216f2177b9a52438e25328dbd99aeec
Maxim Prikhodko <max01kz@gmail.com>
2017-03-16 01:23:18 +0000

    Add updated function to profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,5 @@
 class ProfilesController < ApplicationController
+   before_action :authenticate_user!
    def new
     # Form where a user can fill out their own profile.
     @user = User.find( params[:user_id] )
@@ -19,6 +20,17 @@ class ProfilesController < ApplicationController
    def edit
     @user = User.find( params[:user_id] )
     @profile = @user.profile
+   end
+   
+   def update
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+    if @profile.update_attributes(profile_params)
+     flash[:success] = "Profile Updated!"
+     redirect_to user_path( params[:user_id] )
+    else
+     render action: :edit
+    end
    end
    
    private
